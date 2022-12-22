2d531d4673c7f2651fb9926ef7b8e31bf750318b
Blake <blakepetersont@gmail.com>
2022-12-21 21:30:46 -0700

    started signup.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,1 +1,15 @@
 import React, { useState } from 'react';
+
+import { createUser } from '../utils/API';
+import Auth from '../utils/auth';
+
+const Signup = () => {
+    const [userData, setUserData] = useState({ username: '', email: '', password: ''});
+    const [validated] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const handleInput = (event) => {
+        const { user_name, value } = event.target;
+        setUserData({ ...userData, [user_name]: value});
+    };
+}
