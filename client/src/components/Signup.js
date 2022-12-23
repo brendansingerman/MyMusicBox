f1c95ea7dfc54bdd2a361c1ba2bb91515a93ebdc
Blake <blakepetersont@gmail.com>
2022-12-22 18:54:32 -0700

    finished the js side of the signup.js

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Form } from 'react-bootstrap/lib/Navbar';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -12,4 +13,43 @@ const Signup = () => {
         const { user_name, value } = event.target;
         setUserData({ ...userData, [user_name]: value});
     };
+
+    const handlesubmit = async (event) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+    }
+
+    try {
+        const response = await createUser(userData)
+
+        if (!response.ok) {
+            throw new Error('Something is wrong!');
+        }
+
+        const { token, user } = await response.json();
+        console.log(user);
+        Auth.login(token);
+      } catch (err) {
+        console.error(err);
+        setShowAlert(true);
+    }
+
+    setUserData({
+        username: '',
+        email: '',
+        password: '',
+      });
+}
+
+return (
+    <>
+    <Form noValidate validated={validated} onSubmit={handlesubmit}>
+    <Alert></Alert>
+    </Form>
+    </>
+)
 }
