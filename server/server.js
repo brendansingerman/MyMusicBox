6ec4ddd09c6715c8a7eed640fd4d35aa37d5de4e
unknown <zcfshr@gmail.com>
2022-12-29 15:38:41 -0700

    schemas, mutations and queries

--- server/server.js
+++ server/server.js
@@ -16,6 +16,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+
+
 db.once('open', () => {
+
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
